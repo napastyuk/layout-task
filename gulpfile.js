fe2f8c2ec1dd41200b7f57a2f6a00bb50a812f19
ilya <napastyuk@live.ru>
2019-06-09 02:01:49 +0300

    fransfer site from app to root directory

--- gulpfile.js
+++ gulpfile.js
@@ -5,31 +5,31 @@ var gulp = require('gulp'), // Подключаем Gulp
 gulp.task('browser-sync', function () { // Создаем таск browser-sync
     browserSync({ // Выполняем browserSync
         server: { // Определяем параметры сервера
-            baseDir: 'app' // Директория для сервера - app
+            baseDir: "./" //определяем папку для сервера
         },
         notify: false // Отключаем уведомления
     });
 });
 
 gulp.task('code', function () {
-    return gulp.src('app/*.html')
+    return gulp.src('*.html')
         .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('css', function () {
-    return gulp.src('app/css/*.css')
+    return gulp.src('css/*.css')
         .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('css-prefix', function () {
-    return gulp.src('app/css/*.css') // Выбираем файл для минификации
+    return gulp.src('css/*.css') // Выбираем файл для минификации
         .pipe(autoprefixer(['last 2 versions', '> 1%'], { cascade: true })) // Создаем префиксы
-        .pipe(gulp.dest('app/css')) // Выгружаем в папку app/css
+        .pipe(gulp.dest('css')) // Выгружаем в папку css
 });
 
 gulp.task('watch', function () {
-    gulp.watch('app/css/**/*.css', gulp.parallel('css')); // Наблюдение за sass файлами
-    gulp.watch('app/*.html', gulp.parallel('code')); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('css/**/*.css', gulp.parallel('css')); // Наблюдение за sass файлами
+    gulp.watch('*.html', gulp.parallel('code')); // Наблюдение за HTML файлами в корне проекта
 });
 
 gulp.task('default', gulp.parallel('browser-sync', 'watch'));
